0c31494d0506713b288a5a0ef04c676149d2142f
Elisabeth <Elisabeth@172.18.196.179>
2017-01-18 12:31:02 +0100

    Interface principalement terminée, début d'affectation de bouton

--- src/ihm/javafx/cuisine/projet/FXMLRecetteController.java
+++ src/ihm/javafx/cuisine/projet/FXMLRecetteController.java
@@ -7,7 +7,10 @@ package ihm.javafx.cuisine.projet;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
 /**
  * FXML Controller class
@@ -18,7 +21,25 @@ public class FXMLRecetteController implements Initializable {
 
     /**
      * Initializes the controller class.
-     */
+    */
+    @FXML
+    /*Recupération des objets du fxml*/
+    private TextArea Ingredients;
+    private Button ModifRecetteButton;
+    private Button SaveRecetteButton;
+    
+    @FXML
+    public void modifRecette(ActionEvent e){
+        //ModifRecetteButton.setVisible(false);
+        SaveRecetteButton.setVisible(true);
+        //Ingredients.setEditable(true);
+    }
+    
+    public void saveRecette(ActionEvent e){
+        ModifRecetteButton.setVisible(true);
+        SaveRecetteButton.setDisable(true);
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
