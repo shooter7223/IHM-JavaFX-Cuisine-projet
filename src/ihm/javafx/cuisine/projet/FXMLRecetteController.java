9225ec8dd68a906774f1c4ac5a2d1fe17a784045
Elisabeth <Elisabeth@172.18.196.179>
2017-01-19 16:11:22 +0100

    Classe Recette créée avec qques méthodes, initialisation d'une recette et affichage de ses attributs dans la vue

--- src/ihm/javafx/cuisine/projet/FXMLRecetteController.java
+++ src/ihm/javafx/cuisine/projet/FXMLRecetteController.java
@@ -6,11 +6,15 @@
 package ihm.javafx.cuisine.projet;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
 
 /**
  * FXML Controller class
@@ -23,26 +27,76 @@ public class FXMLRecetteController implements Initializable {
      * Initializes the controller class.
     */
     @FXML
-    /*Recupération des objets du fxml*/
-    private TextArea Ingredients;
-    private Button ModifRecetteButton;
-    private Button SaveRecetteButton;
+    private TextArea IngredientDisplay;
+    @FXML
+    private TextArea MaterielDisplay;
+    @FXML
+    private TextArea ListeDisplay;
+    @FXML
+    private TextArea TempsDisplay;
+    @FXML
+    private Button modifRecetteButton;
+    @FXML
+    private Button saveRecetteButton;
+    @FXML
+    private TextArea InstrucTextArea;
+    @FXML
+    private ImageView ImagePrincipale;
+    @FXML
+    private Image testImg;
     
     @FXML
     public void modifRecette(ActionEvent e){
-        //ModifRecetteButton.setVisible(false);
-        SaveRecetteButton.setVisible(true);
-        //Ingredients.setEditable(true);
+        saveRecetteButton.setVisible(true);
+        modifRecetteButton.setVisible(false);
+        modifEdition(true);
     }
     
+    @FXML
     public void saveRecette(ActionEvent e){
-        ModifRecetteButton.setVisible(true);
-        SaveRecetteButton.setDisable(true);
+        modifRecetteButton.setVisible(true);
+        saveRecetteButton.setVisible(false);
+        modifEdition(false);
+    }
+    
+    @FXML
+    public void supprRecette(ActionEvent e){
+        //TODO
+    }
+    
+    public void modifEdition(boolean bool){
+        IngredientDisplay.setEditable(bool);
+        InstrucTextArea.setEditable(bool);
+        TempsDisplay.setEditable(bool);
+        ListeDisplay.setEditable(bool);
+        MaterielDisplay.setEditable(bool);
+    }
+    
+    @FXML
+    public void InitRecette(Recette r){
+        IngredientDisplay.setText(r.getAllIngredients());
+        MaterielDisplay.setText(r.getAllMateriels());
+        ListeDisplay.setText(r.getAllListe());
+        if(r.getPictures() != null){
+            Image pic = new Image("file:smallCookie.png");
+            ImagePrincipale.setImage(pic);
+        }
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        List<String> ing = new ArrayList<String>();
+        List<String> mat = new ArrayList<String>();
+        List<String> lis = new ArrayList<String>();
+        ing.add("oeuf");
+        mat.add("spatule");
+        lis.add("dessert");
+        List<String> pic = new ArrayList<String>();
+        pic.add("file:smallCookie.png");
+        
+        Recette re = new Recette("cookie", ing, mat, lis, 30.5, pic);
+        InitRecette(re);
     }    
     
 }
---
+++ src/ihm/javafx/cuisine/projet/Recette.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ihm.javafx.cuisine.projet;
+
+import java.util.*;
+import javafx.util.Duration;
+
+/**
+ *
+ * @author Elisabeth
+ */
+public class Recette {
+    private String nom;
+    private List<String> ingredients = new ArrayList<String>();
+    private List<String> materiels = new ArrayList<String>();
+    private List<String> liste = new ArrayList<String>();
+    private Duration duree;
+    private List<String> pictures = new ArrayList<String>();
+    
+    public Recette(String n, List<String> i, List<String> m, List<String> l, double d, List<String> p){
+        nom = n;
+        ingredients = i;
+        materiels = m;
+        liste = l;
+        duree.minutes(d);
+        pictures = p;
+    }
+    
+    public Recette(String n, List<String> i, List<String> m, List<String> l, double d){
+        nom = n;
+        ingredients = i;
+        materiels = m;
+        liste = l;
+        duree.minutes(d);
+    }
+    
+    /*Methods*/
+    List<String> getIngredients(){
+        return ingredients;
+    }
+    
+    String getAllIngredients(){
+        String all = "Ingredients : \n";
+        for(String i : ingredients){
+            all+= i + '\n';
+        }
+        return all;
+    }
+    
+    void setAddIngredient(String s){
+        if(!(s.contains("ngredient")&&s.contains("ngrédient")))
+            ingredients.add(s);
+    }
+    
+    List<String> getMateriels(){
+        return ingredients;
+    }
+    
+    String getAllMateriels(){
+        String all = "Matériels : \n";
+        for(String i : materiels){
+            all+= i + '\n';
+        }
+        return all;
+    }
+    
+    void setAddMateriel(String s){
+        if(!(s.contains("ateriel")&&s.contains("atériel")))
+            materiels.add(s);
+    }
+    
+        List<String> getListe(){
+        return ingredients;
+    }
+    
+    String getAllListe(){
+        String all = "Listes : \n";
+        for(String i : liste){
+            all+= i + '\n';
+        }
+        return all;
+    }
+    
+    void setAddListe(String s){
+        if(!(s.contains("liste")&&s.contains("Liste")))
+            materiels.add(s);
+    }
+   
+    String getNom(){
+        return nom;
+    }
+    
+    void setNom(String n){
+        nom = n;
+    }
+    
+    List<String> getPictures(){
+        return pictures;
+    }
+    
+    String getMainPicture(){
+        /*if(pictures != null){
+            return pictures.get(0);
+        }
+        else
+            return "erreur, image manquante";*/
+        return pictures.get(0);
+    }
+    
+    void setAddPicture(String s){
+        pictures.add(s);
+    }
+}
